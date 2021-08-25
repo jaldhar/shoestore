387dffaf92bfb87f56be457f0aada68dfb35c5b8
Jaldhar H. Vyas <jaldhar@braincells.com>
2021-08-25 06:33:40 -0400

    Added navigation.

--- shoestore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ shoestore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,29 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         Timber.plant(Timber.DebugTree())
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
