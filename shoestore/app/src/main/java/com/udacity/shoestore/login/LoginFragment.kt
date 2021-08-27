0a113fd3b70fbddccdc1b2c381efe2520158bb24
Jaldhar H. Vyas <jaldhar@braincells.com>
2021-08-27 12:48:58 -0400

    Completed Login Screen.

--- shoestore/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ shoestore/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     companion object {
         fun newInstance() = LoginFragment()
@@ -16,7 +25,17 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.loginButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+        )
+
+        binding.signupButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+        )
+
+        return binding.root
     }
 }
